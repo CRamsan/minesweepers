19ec4e7700bfe1115221a06e0b0ef46150e49abe
CRamsan <me@cramsan.com>
2023-04-12 13:37:11 -0700

    Updating versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,23 +6,18 @@ pluginManagement {
     }
 
     plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
-        val detektVersion = extra["detekt.version"] as String
-        val versionsVersion = extra["versions.version"] as String
         val refreshVersion = extra["refresh.version"] as String
 
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
-        id("com.android.base").version(agpVersion)
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-        id("org.jetbrains.compose").version(composeVersion)
-        id("io.gitlab.arturbosch.detekt").version(detektVersion)
-        id("com.github.ben-manes.versions").version(versionsVersion)
+        kotlin("jvm")
+        kotlin("multiplatform")
+        kotlin("android")
         id("de.fayard.refreshVersions").version(refreshVersion)
+        id("com.android.base")
+        id("com.android.application")
+        id("com.android.library")
+        id("org.jetbrains.compose")
+        id("io.gitlab.arturbosch.detekt")
+        id("com.github.ben-manes.versions")
     }
 }
 
